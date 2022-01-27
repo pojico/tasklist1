1a30cf0eab99a37b6add6904fa9ff1537bb6b88c
yuka.tojima <y.tojima04@gmail.com>
2022-01-27 15:13:56 +0900

    Add Servlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -26,8 +26,6 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
     }
 
 }
